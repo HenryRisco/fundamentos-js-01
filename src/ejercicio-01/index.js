305cfaecf58ce93358c9d7ee3ede50b1bc5f1062
HenryRisco <lhrisco@gmail.com>
2024-08-04 19:53:15 +0200

    Añadir ejercicio1

---
+++ src/ejercicio-01/index.js
@@ -0,0 +1,24 @@
+// Este es mi primer programa en Javascript
+const nombre = 'Henry'
+
+const edad = 54
+
+const esEstudiante = true
+
+const mensaje = 'Hola, mi nombre es ' + nombre + ' y tengo ' + edad + ' años. Soy estudiante: ' + esEstudiante + '.'
+
+console.log('Mensaje:', mensaje)
+
+const num1 = 10
+
+const num2 = 5
+
+const suma = num1 + num2
+const resta = num1 - num2
+const multiplicacion = num1 * num2
+const division = num1 / num2
+
+console.log('Suma: ', suma)
+console.log('Resta: ', resta)
+console.log('Multiplicación: ', multiplicacion)
+console.log('División: ', division)
