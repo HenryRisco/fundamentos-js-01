664a83f16e35100a8be6d51426a13a3b299085ea
HenryRisco <lhrisco@gmail.com>
2024-08-08 21:21:39 +0200

    Añadir ejercicio 03

---
+++ src/ejercicio-03/index.js
@@ -0,0 +1,11 @@
+let numero = 3
+
+const tipoNumero = numero > 0 ? 'positivo' : 'negativo o cero'
+
+console.log(tipoNumero)
+
+let cadena = 'Soy Henry'
+
+const longitudCadena = cadena.length > 5 ? 'larga' : 'corta'
+
+console.log(longitudCadena)
